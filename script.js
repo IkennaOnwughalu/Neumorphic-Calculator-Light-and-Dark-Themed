fcb34009ab977b6585a850cfc63ce8c88cd612df
Ikecoz <48351563+IkennaOnwughalu@users.noreply.github.com>
2026-04-20 12:07:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+var keys = document.querySelectorAll("#calc button"); // ✅ was "span", now "button"
+var operators = ["+", "-", "x", "÷"];
+var decimalAdded = false;
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function (e) {
+    var input = document.querySelector(".display");
+    var inputVal = input.innerHTML;
+    var btnVal = this.innerHTML;
+
+    if (btnVal == "C") {
+      input.innerHTML = "";
+      decimalAdded = false;
+
+    } else if (btnVal == "+/-") {
+      // Toggle negative/positive
+      if (inputVal !== "" && !isNaN(inputVal)) {
+        input.innerHTML = (parseFloat(inputVal) * -1).toString();
+      }
+
+    } else if (btnVal == "%") {
+      // Convert to percentage
+      if (inputVal !== "" && !isNaN(inputVal)) {
+        input.innerHTML = (parseFloat(inputVal) / 100).toString();
+        decimalAdded = true;
+      }
+
+    } else if (btnVal == "=") {
+      var equation = inputVal;
+      var lastChar = equation[equation.length - 1];
+
+      equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
+
+      if (operators.indexOf(lastChar) > -1 || lastChar == ".")
+        equation = equation.replace(/.$/, "");
+
+      if (equation) {
+        try {
+          input.innerHTML = Function('"use strict"; return (' + equation + ')')();
+        } catch (err) {
+          input.innerHTML = "Error";
+        }
+      }
+
+      decimalAdded = false;
+
+    } else if (operators.indexOf(btnVal) > -1) {
+      var lastChar = inputVal[inputVal.length - 1];
+
+      if (inputVal != "" && operators.indexOf(lastChar) == -1)
+        input.innerHTML += btnVal;
+      else if (inputVal == "" && btnVal == "-")
+        input.innerHTML += btnVal;
+
+      if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+        input.innerHTML = inputVal.replace(/.$/, btnVal);
+      }
+
+      decimalAdded = false;
+
+    } else if (btnVal == ".") {
+      if (!decimalAdded) {
+        input.innerHTML += btnVal;
+        decimalAdded = true;
+      }
+
+    } else {
+      input.innerHTML += btnVal;
+    }
+
+    e.preventDefault();
+  };
+}
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
